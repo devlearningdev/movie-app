9c068c2f6bdf231bd008cd9ca794b16c0bdd5829
devlearningdev <110401395+devlearningdev@users.noreply.github.com>
2022-09-03 11:46:34 +0200

    adding official trailer with youtube redirection

--- src/Components/MovieDetails.js
+++ src/Components/MovieDetails.js
@@ -5,9 +5,39 @@ import TrendingMovies from "./TrendingMovies";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
 
 const MovieDetails = () => {
   const location = useLocation();
+  const [trailers, setTrailers] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${location.state.id}/videos?api_key=b28a53f206c15a63a8c1de7477017045&language=en-US`
+      )
+      .then((res) => setTrailers(res.data.results));
+  }, []);
+
+  console.log(trailers);
+
+  const officialTrailer = trailers.map((item) => {
+    {
+      return (
+        item.name.match(/^Official Trailer$/) && (
+          <iframe
+            width="100%"
+            height="315"
+            src={`https://www.youtube.com/embed/${item.key}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        )
+      );
+    }
+  });
 
   return (
     <div>
@@ -34,6 +64,10 @@ const MovieDetails = () => {
               <u>{location.state.reviewNumber} reviews</u>)
             </span>
           </p>
+          <br />
+
+          <br />
+          {officialTrailer}
         </div>
       </div>
     </div>
