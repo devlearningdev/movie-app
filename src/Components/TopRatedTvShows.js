8e2af6bad1576be8615f1a9c174da6dad8374ad9
devlearningdev <110401395+devlearningdev@users.noreply.github.com>
2022-09-07 17:51:41 +0200

    app V2: new sliders, major upgrade

--- src/Components/TopRatedTvShows.js
+++ src/Components/TopRatedTvShows.js
@@ -1,6 +1,9 @@
 import React from "react";
 import axios from "axios";
 import Card from "./Card";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const TopRatedTvShows = () => {
   const [topRated, setTopRated] = React.useState([]);
@@ -18,12 +21,135 @@ const TopRatedTvShows = () => {
     return <Card key={item.id} item={item} />;
   });
 
+  function SimpleSlider() {
+    var settings = {
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 15,
+      slidesToScroll: 15,
+      responsive: [
+        {
+          breakpoint: 3200,
+          settings: {
+            slidesToShow: 13,
+            slidesToScroll: 13,
+          },
+        },
+        {
+          breakpoint: 2800,
+          settings: {
+            slidesToShow: 11,
+            slidesToScroll: 11,
+          },
+        },
+        {
+          breakpoint: 2435,
+          settings: {
+            slidesToShow: 10,
+            slidesToScroll: 10,
+          },
+        },
+        {
+          breakpoint: 2200,
+          settings: {
+            slidesToShow: 9,
+            slidesToScroll: 9,
+          },
+        },
+        {
+          breakpoint: 1990,
+          settings: {
+            slidesToShow: 8,
+            slidesToScroll: 8,
+          },
+        },
+        {
+          breakpoint: 1780,
+          settings: {
+            slidesToShow: 7,
+            slidesToScroll: 7,
+          },
+        },
+        {
+          breakpoint: 1595,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 6,
+          },
+        },
+        {
+          breakpoint: 1350,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+          },
+        },
+        {
+          breakpoint: 1210,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 6,
+          },
+        },
+        {
+          breakpoint: 1100,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+          },
+        },
+        {
+          breakpoint: 950,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+          },
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+
+        {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+        /*{
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },*/
+      ],
+    };
+    return <Slider {...settings}>{movieToShow}</Slider>;
+  }
+
   return (
     <div className="top-rated-container">
       <div className="title-div">
         <h3>⭐ Top Rated</h3>
       </div>
-      <div className="trending-movies-container">{movieToShow}</div>
+      {/*<div className="trending-movies-container">{movieToShow}</div>*/}
+      <div className="slider-container">
+        <SimpleSlider />
+      </div>
     </div>
   );
 };
--- src/Components/TrendingMovies.js
+++ src/Components/TrendingMovies.js
@@ -2,6 +2,9 @@ import React from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Card from "./Card";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const TrendingMovies = () => {
   const [trendingMovies, setTrendingMovies] = React.useState([]);
@@ -19,12 +22,135 @@ const TrendingMovies = () => {
     return <Card key={item.id} item={item} />;
   });
 
+  function SimpleSlider() {
+    var settings = {
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 15,
+      slidesToScroll: 15,
+      responsive: [
+        {
+          breakpoint: 3200,
+          settings: {
+            slidesToShow: 13,
+            slidesToScroll: 13,
+          },
+        },
+        {
+          breakpoint: 2800,
+          settings: {
+            slidesToShow: 11,
+            slidesToScroll: 11,
+          },
+        },
+        {
+          breakpoint: 2435,
+          settings: {
+            slidesToShow: 10,
+            slidesToScroll: 10,
+          },
+        },
+        {
+          breakpoint: 2200,
+          settings: {
+            slidesToShow: 9,
+            slidesToScroll: 9,
+          },
+        },
+        {
+          breakpoint: 1990,
+          settings: {
+            slidesToShow: 8,
+            slidesToScroll: 8,
+          },
+        },
+        {
+          breakpoint: 1780,
+          settings: {
+            slidesToShow: 7,
+            slidesToScroll: 7,
+          },
+        },
+        {
+          breakpoint: 1595,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 6,
+          },
+        },
+        {
+          breakpoint: 1350,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+          },
+        },
+        {
+          breakpoint: 1210,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 6,
+          },
+        },
+        {
+          breakpoint: 1100,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+          },
+        },
+        {
+          breakpoint: 950,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+          },
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+
+        {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+        /*{
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },*/
+      ],
+    };
+    return <Slider {...settings}>{movieToShow}</Slider>;
+  }
+
   return (
     <div>
       <div className="title-div">
         <h3>🚀 Trending this week</h3>
       </div>
-      <div className="trending-movies-container">{movieToShow}</div>
+      {/*<div className="trending-movies-container">{movieToShow}</div>*/}
+      <div className="slider-container">
+        <SimpleSlider />
+      </div>
     </div>
   );
 };
