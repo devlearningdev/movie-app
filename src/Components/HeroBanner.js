3aace503217e72c5b3494892bd450ae9132e4e58
devlearningdev <110401395+devlearningdev@users.noreply.github.com>
2022-10-14 15:23:16 +0200

    hero banner integration

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -1,7 +1,19 @@
 import React from "react";
+import axios from "axios";
 
 const HeroBanner = () => {
-  return <div className="hero-container"></div>;
+  return (
+    <div className="hero-container">
+      <img
+        className="hero-img"
+        src="https://www.xtrafondos.com/wallpapers/personajes-de-temporada-4-stranger-things-10323.jpg"
+      ></img>
+      <div className="hero-text">
+        <h1>Search Thousands of movies.</h1>
+        <p>Look for trending content, up-coming movies, top rated etc ...</p>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
