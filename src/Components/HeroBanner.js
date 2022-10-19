564f3339fbcd68929422a2a130076f77f22b4030
devlearningdev <110401395+devlearningdev@users.noreply.github.com>
2022-10-19 15:28:50 +0200

    hero background

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -2,15 +2,42 @@ import React from "react";
 import axios from "axios";
 
 const HeroBanner = () => {
+  const [trendingMovies, setTrendingMovies] = React.useState([]);
+  const [displayWall, setDisplayWall] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/trending/all/week?api_key=b28a53f206c15a63a8c1de7477017045"
+      )
+      .then((res) => {
+        setTrendingMovies(res.data.results);
+      });
+  }, []);
+
+  const wallpapers = trendingMovies.map((item) => item.backdrop_path);
+
+  function randomPick() {
+    return wallpapers[Math.floor(Math.random() * wallpapers.length)];
+  }
+
+  setInterval(randomPick, 5000);
+
+  //setInterval("console.log('eagle')", 5000);
+
+  /*const wallpaperPath = setInterval(function random() {
+    trendingMovies.map((item) => console.log(item.backdrop_path));
+  }, 5000);*/
+
   return (
     <div className="hero-container">
       <img
         className="hero-img"
-        src="https://www.xtrafondos.com/wallpapers/personajes-de-temporada-4-stranger-things-10323.jpg"
+        src={`https://image.tmdb.org/t/p/original${randomPick()}`}
       ></img>
       <div className="hero-text">
-        <h1>Search Thousands of movies.</h1>
-        <p>Look for trending content, up-coming movies, top rated etc ...</p>
+        <h1 className="main-h1">Search Thousands of movies.</h1>
+        {/*<p>Look for trending content, up-coming movies, top rated etc ...</p>*/}
       </div>
     </div>
   );
